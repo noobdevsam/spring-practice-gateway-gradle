f1b61383bce83bc6f1c9aaf287ba5c802dfeb9c2
noobdevsam <mdshoykotislam05@gmail.com>
2025-06-02 20:49:30 +0600

    first commit

---
+++ src/main/java/com/example/spring_practice_gateway_gradle/SpringPracticeGatewayGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_gateway_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeGatewayGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeGatewayGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_practice_gateway_gradle/SpringPracticeGatewayGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_practice_gateway_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeGatewayGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
